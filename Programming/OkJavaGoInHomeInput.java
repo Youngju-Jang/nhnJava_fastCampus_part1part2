a337218925a5f6e0ae9a842e290f84526f40291f
Youngju-Jang <joj1043@users.noreply.github.com>
2022-04-19 00:24:58 +0900

    cmd로 arg추가하여 실행하기 주석

--- Programming/OkJavaGoInHomeInput.java
+++ Programming/OkJavaGoInHomeInput.java
@@ -1,12 +1,17 @@
+//import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Lighting;
 import org.opentutorials.iot.Security;
 
-public class OkJavaGoInHome {
+public class OkJavaGoInHomeInput {
 
+	// parameter, 매개변수 >> Run configuration > arguments 에서 설정가능
 	public static void main(String[] args) {
 		
-		String id = "JAVA APT 507";
+		String id = args[0];
+		String bright = args[1];
 		
 		// TODO Auto-generated method stub
 		// Elevator call
@@ -24,10 +29,9 @@ public class OkJavaGoInHome {
 		Lighting floorLamp = new Lighting(id+" / Floor Lamp");
 		floorLamp.on();
 		
-		// org패키지를 신규 lib패키지밑으로 넣었을 경우
-		// cmd로 실행 >> javac -cp ".;lib" OkJavaGoInHome.java
-		// javac -cp ".;Programming/" Programming/OkJavaGoInHomeInput.java >> 이리하니 오류가안뜸
-		// java class못찾는 오류 : %JAVA_HOME%\lib;.  << ;. 추가하기
+		DimmingLights moodLamp = new DimmingLights(id+"moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
 
 	}
 
