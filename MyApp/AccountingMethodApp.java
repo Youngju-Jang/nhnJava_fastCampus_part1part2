94c51e647d8f8f33024f3252131f3d5ddba77a0c
Youngju-Jang <joj1043@users.noreply.github.com>
2022-04-19 22:59:48 +0900

    배열추가, 메소드추가

---
+++ MyApp/AccountingMethodApp.java
@@ -0,0 +1,31 @@
+
+public class AccountingMethodApp {
+
+	public static void main(String[] args) {
+		
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double expenseRate = 0.3;
+		double vatRate = 0.1;
+		double vat = valueOfSupply*vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply*expenseRate;
+		double income = valueOfSupply - expense;
+		double divided1 = income * 0.5;
+		double divided2 = income * 0.3;
+		double divided3 = income * 0.2;
+		
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + vat );
+		System.out.println("Total : " + total );
+		System.out.println("Expense : " + expense );
+		System.out.println("Income : " + income );
+		
+		System.out.println("Dividend1: " + divided1 );
+		System.out.println("Dividend2: " + divided2 );
+		System.out.println("Dividend3: " + divided3 );
+		
+		
+		
+	}
+
+}
