8537b4aab6b7aa0449fec842952df22ef835a720
Youngju-Jang <joj1043@users.noreply.github.com>
2022-04-19 23:13:00 +0900

    메소드2

--- MyApp/AccountingMethodApp.java
+++ MyApp/AccountingMethodApp.java
@@ -1,31 +1,65 @@
 
 public class AccountingMethodApp {
 
+	public static double valueOfSupply;
+	public static double vatRate;
+	public static double expenseRate;
 	public static void main(String[] args) {
 		
-		double valueOfSupply = Double.parseDouble(args[0]);
-		double expenseRate = 0.3;
-		double vatRate = 0.1;
-		double vat = valueOfSupply*vatRate;
-		double total = valueOfSupply + vat;
-		double expense = valueOfSupply*expenseRate;
-		double income = valueOfSupply - expense;
-		double divided1 = income * 0.5;
-		double divided2 = income * 0.3;
-		double divided3 = income * 0.2;
+		valueOfSupply = 10000.0;
+		vatRate = 0.1;
+		expenseRate = 0.3;
+		double vat = getVAT();
+		double total = getTotal();
+		double expense = getExpense();
+		double income = getIncome();
+		double divided1 = getDividend1();
+		double divided2 = getDividend2();
+		double divided3 = getDividend3();
 		
+		print();
+		
+		
+		
+	}
+
+	private static void print() {
 		System.out.println("Value of supply : " + valueOfSupply);
-		System.out.println("VAT : " + vat );
-		System.out.println("Total : " + total );
-		System.out.println("Expense : " + expense );
-		System.out.println("Income : " + income );
-		
-		System.out.println("Dividend1: " + divided1 );
-		System.out.println("Dividend2: " + divided2 );
-		System.out.println("Dividend3: " + divided3 );
-		
-		
-		
+		System.out.println("VAT : " + getVAT() );
+		System.out.println("Total : " + getTotal() );
+		System.out.println("Expense : " + getExpense() );
+		System.out.println("Income : " + getIncome() );		
+		System.out.println("Dividend1: " + getDividend1() );
+		System.out.println("Dividend2: " + getDividend2() );
+		System.out.println("Dividend3: " + getDividend3() );
+	}
+
+	private static double getDividend1() {
+		return getIncome() * 0.5;
+	}
+
+	private static double getDividend2() {
+		return getIncome() * 0.3;
+	}
+	
+	private static double getDividend3() {
+		return getIncome() * 0.2;
+	}
+	
+	private static double getIncome() {
+		return valueOfSupply - getExpense();
+	}
+
+	private static double getExpense() {
+		return valueOfSupply*expenseRate;
+	}
+
+	private static double getTotal() {
+		return valueOfSupply + getVAT();
+	}
+
+	private static double getVAT() {
+		return valueOfSupply*vatRate;
 	}
 
 }
